5cc30e538c1d02d9cbb02e27aad6a453a80f9549
Marcio Lopes <marcitolopes@gmail.com>
2009-08-27 18:32:29 -0300

    added rack and some stuffs in pages.yml

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
   config.gem "simplificator-rwebthumb", :source => "http://gems.github.com", :lib => "rwebthumb" 
+  config.gem "rack", :version => "1.0.0"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
