13caf01b4bc11fd40384b8d962b9d4d0fa34851a
Marcio Lopes <marcitolopes@gmail.com>
2009-08-27 19:11:05 -0300

    added tag_cloud method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
 
-    include TagsHelper
-
     def validate_i18n
       "plugins/validate_" + I18n.locale
       rescue
@@ -18,5 +16,15 @@ module ApplicationHelper
 
     def tag_classes
       %w(tag1 tag2 tag3 tag4 tag5 tag6 tag7 tag8 tag9 tag10)
+    end
+
+    def tag_cloud(tags, classes)
+      max_count = tags.sort_by(&:count).last.count.to_f rescue 1.0
+      
+      tags.each do |tag|
+        index = ((tag.count / max_count) * (classes.size - 1)).round
+        debugger
+        yield tag, classes[index]
+      end
     end
 end
