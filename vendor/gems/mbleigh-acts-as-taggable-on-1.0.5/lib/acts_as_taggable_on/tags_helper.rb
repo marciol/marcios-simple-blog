72ba37a24708effd07bacb05560e1db39f382b3f
Marcio Lopes <marcitolopes@gmail.com>
2009-08-27 18:57:44 -0300

    fixed a bug in tag_cloud

--- vendor/gems/mbleigh-acts-as-taggable-on-1.0.5/lib/acts_as_taggable_on/tags_helper.rb
+++ vendor/gems/mbleigh-acts-as-taggable-on-1.0.5/lib/acts_as_taggable_on/tags_helper.rb
@@ -1,10 +1,11 @@
 module TagsHelper
   # See the README for an example using tag_cloud.
   def tag_cloud(tags, classes)
-    max_count = tags.sort_by(&:count).last.count.to_f
+    max_count = tags.sort_by(&:count).last.count.to_f rescue 1.0
     
     tags.each do |tag|
       index = ((tag.count / max_count) * (classes.size - 1)).round
+      debugger
       yield tag, classes[index]
     end
   end
